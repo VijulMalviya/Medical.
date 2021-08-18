9a7e8409e25e800cae3b34736886e71593fe9b74
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2021-08-18 14:50:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function service(evt, servicedepartment) {
+    var i, tabcontent, servicesicon;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    servicesicon = document.getElementsByClassName("servicesicon");
+    for (i = 0; i < servicesicon.length; i++) {
+        servicesicon[i].className = servicesicon[i].className.replace(" active", "");
+    }
+    document.getElementById(servicedepartment).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
